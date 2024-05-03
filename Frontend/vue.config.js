f3cf7b0a3123a672bbdbaa9101899734a3e53b1c
roger <nighterdream123@gmail.com>
2024-05-04 03:13:17 +0800

    配置文件

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("@vue/cli-service");
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+// const { defineConfig } = require("@vue/cli-service");
+// const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack(config) {
-    config.plugin("monaco").use(new MonacoWebpackPlugin());
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   chainWebpack(config) {
+//     config.plugin("monaco").use(new MonacoWebpackPlugin());
+//   },
+//   configureWebpack: {
+//     devtool: "source-map",
+//   },
+// });
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
   },
-});
+};
